0dc2b6c4592c64cce7547878ee04d57beb0a037b
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-01 12:34:25 +0530

    Add fevicon and tab name

--- src/components/airport/Airport.tsx
+++ src/components/airport/Airport.tsx
@@ -5,7 +5,7 @@ export default function Airports() {
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
               alt="Your Company"
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+              src="https://files.softicons.com/download/web-icons/vista-poi-icons-by-icons-land/ico/AirportBlue.ico"
               className="mx-auto h-10 w-auto"
             />
             <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
