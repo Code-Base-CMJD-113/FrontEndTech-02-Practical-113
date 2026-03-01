b0bd439f9d07f8175030c125015ee29eb4534edd
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-01 13:47:38 +0530

    Add service layer

---
+++ src/components/service/AirportService.ts
@@ -0,0 +1,24 @@
+import axios from "axios"
+
+const baseUrl = "http://localhost:8080/air//api/v1/airports"
+const saveAirport = async (airportData : any) =>{
+    try{
+     const response = await axios.post(baseUrl,airportData)
+     return response.status
+    }catch(err){
+        console.error(err)
+    }
+   
+  
+}
+const getAirports = async () =>{
+    
+}
+const deleteAirport = async () =>{
+    
+}
+const updateAirport = async () =>{
+    
+}
+
+export { saveAirport, getAirports, deleteAirport, updateAirport}
