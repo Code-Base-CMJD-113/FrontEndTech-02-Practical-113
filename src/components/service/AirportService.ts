ed6208edd95db9fc08d0a325ba19df1e6771c09d
ThanuraSilva <thanurasilva1989@gmail.com>
2026-03-15 11:19:09 +0530

    Test airport service as secured

--- src/components/service/AirportService.ts
+++ src/components/service/AirportService.ts
@@ -1,16 +1,35 @@
 import axios from "axios"
 
 const baseUrl = "http://localhost:8081/air/api/v1/airports"
+const fetchToken = ()=>{
+    const token = localStorage.getItem("cmjd113")
+    return "Bearer "+token;
+}
 const saveAirport = async (airportData : any) =>{
     try{
-     const response = await axios.post(baseUrl,airportData)
+     const response = await axios.post(baseUrl,
+        airportData,
+        {
+            headers: {
+                Authorization: fetchToken()
+            }
+        }
+        )
      return response.status
     }catch(err){
         console.error(err)
     }
 }
 const getAirports = async () =>{
-    const response = await axios.get(baseUrl)
+    const response = await axios.get
+    (baseUrl,
+        {
+            headers:{
+                Authorization: fetchToken()
+            }
+        }
+        
+        )
     return response.data
 
 }
